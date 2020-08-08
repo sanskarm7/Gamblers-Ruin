fd23d0ad74f01a0f0134063b61d2a8ec741f6458
Sanskar <sanskar.mishra@gmail.com>
2020-08-08 13:06:10 -0500

    we set up the initial money and we set up the players goal

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Main
+
 {
     public static void main(String[] args)
     {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("How much money do you want to put in? >>> ");
+      double money = sc.nextDouble();
+      sc.nextLine();
+      System.out.print("How much money are you hoping to end up with today >>> ");
+      double winMoney = sc.nextDouble();
+      sc.nextLine();
 
     }
 }
