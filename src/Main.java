7afea24efd141c81f506c813c56571dea38f94d9
Sanskar Mishra <Sanskar@Sanskars-MBP.attlocal.net>
2020-08-15 13:16:13 -0500

    added a way to put in new rounds and made a way for the player to guess between 1 and 100

--- src/Main.java
+++ src/Main.java
@@ -13,5 +13,34 @@ public class Main
       double winMoney = sc.nextDouble();
       sc.nextLine();
 
+
+
+      int round = 1;
+      while (money > 0 && money < winMoney)
+      {
+          System.out.println("Round " + round + "......");
+          System.out.println("\n\n");
+          System.out.print("Guess a number between 0 and 100 >>> ");
+          int guessedNumber = getKeyboardInt(sc);
+
+
+      }
     }
+public static int getKeyboardInt(Scanner sc)
+{
+    while(true)
+    {
+        try
+        {
+            int guessedNumber = sc.nextInt();
+            sc.nextLine();
+            return guessedNumber;
+        }
+        catch (Exception e)
+        {
+            System.out.println("Not an Integer!");
+        }
+    }
+}
+
 }
